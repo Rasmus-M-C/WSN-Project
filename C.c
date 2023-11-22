85cd236eacd15c8be1cd484d7aa9be7e1b40cad2
Rasmus-M-C <rasmusmcarlsen@gmail.com>
2023-11-22 14:56:30 +0100

    simple A to C with reponse

--- C.c
+++ C.c
@@ -1,15 +1,24 @@
 #include "contiki.h"
+#include "net/routing/routing.h"
+#include "random.h"
+#include "net/netstack.h"
+#include "net/ipv6/simple-udp.h"
+#include <stdint.h>
+#include <inttypes.h>
+
+#include "sys/log.h"
+#include "contiki.h"
 #include "net/ipv6/simple-udp.h"
 #include <stdio.h>
-#include "simple-udp.h"
-#include "net/ipv6/simple-udp.h"
+#include <string.h>
+
 #define UDP_CLIENT_PORT 8765
 #define UDP_SERVER_PORT 5678
 
 static struct simple_udp_connection udp_conn;
 
-PROCESS(udp_server_process, "UDP server");
-AUTOSTART_PROCESSES(&udp_server_process);
+PROCESS(udp_client_process, "UDP client");
+AUTOSTART_PROCESSES(&udp_client_process);
 
 static void udp_rx_callback(struct simple_udp_connection *c,
                             const uip_ipaddr_t *sender_addr,
@@ -17,18 +26,29 @@ static void udp_rx_callback(struct simple_udp_connection *c,
                             const uip_ipaddr_t *receiver_addr,
                             uint16_t receiver_port,
                             const uint8_t *data,
-                            uint16_t datalen) {
-    printf("Received message from Mote A: '%.*s'\n", datalen, (char *)data);
-
-    const char *response = "ACK from C";
-    simple_udp_sendto(&udp_conn, response, strlen(response), sender_addr);
-    printf("Sent ACK to Mote A\n");
+                            uint16_t datalen)
+{
+  // Check if the received message is "test" from mote A
+  if (strncmp((char *)data, "test", 4) == 0) {
+    // Send an acknowledgment back to mote A
+    char ack[] = "ACK from C";
+    simple_udp_sendto(&udp_conn, ack, strlen(ack), sender_addr);
+    printf("Sending ACK to A\n");
+  }
 }
 
-PROCESS_THREAD(udp_server_process, ev, data) {
-    PROCESS_BEGIN();
+PROCESS_THREAD(udp_client_process, ev, data)
+{
+  PROCESS_BEGIN();
 
-    simple_udp_register(&udp_conn, UDP_SERVER_PORT, NULL, UDP_CLIENT_PORT, udp_rx_callback);
+  // Initialize UDP connection
+  simple_udp_register(&udp_conn, UDP_CLIENT_PORT, NULL,
+                      UDP_SERVER_PORT, udp_rx_callback);
 
-    PROCESS_END();
+  while (1) {
+    PROCESS_WAIT_EVENT();
+  }
+
+  PROCESS_END();
 }
+
